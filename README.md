d43063f2dfd1c4bb734982fb1118a6525735f222
IamDinni <IamDinni@gmail.com>
2021-04-06 16:09:55 +0530

    Cleaned the repo

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
-# IEEEhackathon
+<br/>
 
------------------------------------------------------------------------------------------------------------------------------------------
-                                                     
-       Digitized Attendance System (DAS) -  An upgradation of the previously used ordinary human resource based attendance management.
-            
-------------------------------------------------------------------------------------------------------------------------------------------
+<p align="center">
+  <img width="20%" src="public/assets/das.png" alt="Me">
+</p>
 
-# How to contribute
-Well you can contribute to this repository by many ways:
-- Suggest changes/features you want in future releases.
-- Fork the repository, make some additional changes and send me a pull request. 
-- If there's any issues, please ask them and if you're too good to be good, than you can even rectify those issues on your own.
-- Documenting your code is always a good habit. It enhances your code quality and reduces stress of other developers working on your project.
+<h2 align="center">
+<a href="#" rel="noopener" target="_blank">Digitized Attendance System</a>
+</h2>
 
----
+<p align="center">
+  An upgradation of the previously used ordinary human resource based attendance management.
+</p>
 
-# License
+<br/>
+<br/>
+
+### 🤝 Contributing
+
+Well you can contribute to this repository in many ways:
+-  Suggest changes/features you want in future releases.
+-  Fork the repository, make some additional changes and send me a pull request. 
+-  If there's any issues, please ask them and if you're too good to be good, than you can even rectify those issues on your own.
+-  Documenting your code is always a good habit. It enhances your code quality and reduces stress of other developers working on your project.
+
+<br/>
+<br/>
+
+### ⚖️ License
+
 This project is licensed under the MIT License - see the [LICENSE](../blob/main/LICENSE) file for details
--- server.js
+++ server.js
@@ -1,65 +1,65 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId;
-const jwt = require('jwt-simple');
-const bcrypt = require('bcryptjs')
-const app = express();
+import express, { json } from 'express'
+import { MongoClient } from 'mongodb'
+import jwt from 'jwt-simple'
+import bcrypt from "bcryptjs";
+import { config } from 'dotenv'
 
-const JWT_SECRET_KEY = <YOUR_SECRET_KEY>
+const app = express();
+const dot = config();
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
+app.use(json());
 
 var dbconn = null;
 
-MongoClient.connect('mongodb://<USERNAME>:<PASSWORD>@ds261078.mlab.com:61078/das', function(err, db) {
+MongoClient.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds261078.mlab.com:61078/das`, function (err, db) {
     if (err) throw err
     console.log("We are connected!");
     dbconn = db;
-})  
+})
 
 
-
-app.route('/routine').get(function(req, res, next) {
+app.route('/routine').get(function (req, res, next) {
     teachers_data = req.query.username
     lect = req.query.lecture
     day = req.query.day
-    dbconn.collection('routine', { w: 1 }, function(err, routineCollection) {
+    dbconn.collection('routine', { w: 1 }, function (err, routineCollection) {
 
-        routineCollection.find({"lecture": lect, "username": teachers_data, "day": day}).toArray(function(err, routine_data) {
+        routineCollection.find({ "lecture": lect, "username": teachers_data, "day": day }).toArray(function (err, routine_data) {
             if (err) throw err;
             class_name = routine_data[0].class
             console.log(class_name)
-            if(!routine_data.length) return res.send("No data found!!!");
+            if (!routine_data.length) return res.send("No data found!!!");
 
-            dbconn.collection('studentlist', { w: 1 }, function(err, studentlistCollection) {
-                studentlistCollection.find({"class": class_name}).toArray(function(err, studentlist_data) {
+            dbconn.collection('studentlist', { w: 1 }, function (err, studentlistCollection) {
+                studentlistCollection.find({ "class": class_name }).toArray(function (err, studentlist_data) {
                     if (err) throw err;
                     return res.send(studentlist_data);
                 })
             });
-            
-            
+
+
         })
     });
 
 });
 
-app.post('/meows', function(req, res, next) {
-    dbconn.collection('routine', { w: 1 }, function(err, routineCollection) {
+app.post('/attendance', function (req, res, next) {
+    dbconn.collection('routine', { w: 1 }, function (err, routineCollection) {
 
         var token = req.headers.authorization;
         var user = jwt.decode(token, JWT_SECRET_KEY);
 
 
-        var insertMeow = {
-            text: req.body.newMeow,
+        var insertAttendance = {
+            text: req.body.newAttendance,
             user: user[0]._id,
             username: user[0].username
         };
 
-        routineCollection.insertOne(insertMeow, { w: 1 }, function(err, routine) {
+        routineCollection.insertOne(insertAttendance, { w: 1 }, function (err, routine) {
             if (err) throw err;
             return res.json();
         })
@@ -67,19 +67,19 @@ app.post('/meows', function(req, res, next) {
 });
 
 
-app.post('/users', function(req, res, next) {
-    dbconn.collection('teachers', function(err, teachersCollection) {
+app.post('/users', function (req, res, next) {
+    dbconn.collection('teachers', function (err, teachersCollection) {
 
-        teachersCollection.findOne({ username: req.body.username }, function(err, user) {
+        teachersCollection.findOne({ username: req.body.username }, function (err, user) {
             if (err || user == null) {
-                bcrypt.genSalt(10, function(err, salt) {
-                    bcrypt.hash(req.body.password, salt, function(err, hash) {
+                bcrypt.genSalt(10, function (err, salt) {
+                    bcrypt.hash(req.body.password, salt, function (err, hash) {
                         var newUser = {
                             username: req.body.username,
                             password: hash
                         }
 
-                        teachersCollection.insertOne(newUser, { w: 1 }, function(err, teachers) {
+                        teachersCollection.insertOne(newUser, { w: 1 }, function (err, teachers) {
                             if (err) throw err;
                             return res.json();
                         });
@@ -93,12 +93,12 @@ app.post('/users', function(req, res, next) {
     });
 });
 
-app.put('/users/signin', function(req, res, next) {
+app.put('/users/signin', function (req, res, next) {
 
-    dbconn.collection('teachers', function(err, teachersCollection) {
-        teachersCollection.find({ username: req.body.username }).toArray(function(err, user) {
+    dbconn.collection('teachers', function (err, teachersCollection) {
+        teachersCollection.find({ username: req.body.username }).toArray(function (err, user) {
             if (!err) {
-                bcrypt.compare(req.body.password, user[0].password, function(err, result) {
+                bcrypt.compare(req.body.password, user[0].password, function (err, result) {
                     if (!result) {
                         return res.status(400).send();
                     }
@@ -113,6 +113,6 @@ app.put('/users/signin', function(req, res, next) {
     });
 });
 
-app.listen(3000, function() {
-    console.log('App listening on port 3000!')
+app.listen(process.env.PORT || 3000, function () {
+    console.log(`App listening on localhost:${process.env.PORT || 3000}`)
 });
